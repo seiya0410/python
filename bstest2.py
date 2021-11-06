9b391e23e9a8eb02de8f3f9178b5d1cb45aa3194
Seiya <st302519@gmail.com>
2021-11-06 17:33:39 +0900

    add new file

---
+++ bstest2.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+
+html = """
+<html><body>
+    <h1 id="title"> スクレイピングとは</h1>
+    <p id="body">Webページを解析すること</p>
+    </body></html>
+"""
+
+soup = BeautifulSoup(html, 'html.parser')
+
+title = soup.find(id="title")
+body = soup.find(id="body")
+
+print("#title" + title.string)
+print("#body" + body.string)
+
